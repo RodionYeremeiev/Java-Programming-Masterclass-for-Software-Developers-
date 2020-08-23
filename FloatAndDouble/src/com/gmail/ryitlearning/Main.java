4dabf2b948c8190050b3ad65c8c81861370fb7a5
Rodion Yeremeiev <rodion.ieremeiev@gmail.com>
2020-08-23 16:10:36 +0300

    29. Floating oint precision & Challenge

--- FloatAndDouble/src/com/gmail/ryitlearning/Main.java
+++ FloatAndDouble/src/com/gmail/ryitlearning/Main.java
@@ -13,9 +13,24 @@ public class Main {
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value = " + myMaxDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = (float) 5.25;
-        double myDoubleValue = 5.25d;
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5.00 / 3.00;
+
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
+
+//        Challenge:
+        double pounds = 200d;
+        double kilograms = pounds * 0.45359237d;
+        System.out.println(pounds + " pounds equals to " + kilograms + " kilograms");
+
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.4_567_890d;
+        System.out.println(pi);
+        System.out.println(anotherNumber);
+
 
     }
 }
