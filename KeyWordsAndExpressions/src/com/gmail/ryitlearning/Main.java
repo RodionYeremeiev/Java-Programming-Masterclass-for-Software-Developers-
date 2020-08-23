72150e563c460dd05a87701c6f40e51f32ad77d1
Rodion Yeremeiev <rodion.ieremeiev@gmail.com>
2020-08-23 18:38:54 +0300

    42. Keywords and Expressions

---
+++ KeyWordsAndExpressions/src/com/gmail/ryitlearning/Main.java
@@ -0,0 +1,23 @@
+package com.gmail.ryitlearning;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        a mile is equal to  1.609344 kilometres
+        double kilometres = (100 * 1.609344);
+        int highScore = 50;
+
+        if (highScore == 50) {
+            System.out.println("This is sn expression");
+        }
+
+        int score = 100;
+//        score = 100 in line 15 is the expression
+        if (score > 99) {
+            System.out.println("You got the high score!");
+            score = 0;
+        }
+//        everything inside {...} from lines 17-20 are th expression
+    }
+}
