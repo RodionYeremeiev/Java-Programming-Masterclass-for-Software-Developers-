41d84213b7b7227df8308d4ba515e342b40856a8
Rodion Yeremeiev <rodion.ieremeiev@gmail.com>
2020-08-25 15:49:48 +0300

    49. Method Challenge - Final Code Changes

--- IfKeywordsAndCodeBloks/src/com/gmail/ryitlearning/Main.java
+++ IfKeywordsAndCodeBloks/src/com/gmail/ryitlearning/Main.java
@@ -21,10 +21,11 @@ public class Main {
         displayHighScorePosition("player2", calculateHighScorePosition(900));
         displayHighScorePosition("player3", calculateHighScorePosition(400));
         displayHighScorePosition("player4", calculateHighScorePosition(50));
-
+        System.out.println();
         displayHighScorePosition("player5", calculateHighScorePosition(1000));
         displayHighScorePosition("player6", calculateHighScorePosition(500));
         displayHighScorePosition("player7", calculateHighScorePosition(100));
+        displayHighScorePosition("player8", calculateHighScorePosition(45));
     }
 
     public static int calculateScore(boolean gameOver,
@@ -45,14 +46,23 @@ public class Main {
     }
 
     public static int calculateHighScorePosition(int playerScore) {
+//        if (playerScore >= 1000) {
+//            return 1;
+//        } else if (playerScore >= 500) {
+//            return 2;
+//        } else if (playerScore >= 100) {
+//            return 3;
+//        }
+//        return 4;
+        int position = 4; //assuming position 4 will be reteurned
+
         if (playerScore >= 1000) {
-            return 1;
-        } else if (playerScore >= 500 && playerScore < 1000) {
-            return 2;
-        } else if (playerScore >= 100 && playerScore < 500) {
-            return 3;
-        } else {
-            return 4;
+            position = 1;
+        } else if (playerScore >= 500) {
+            position = 2;
+        } else if (playerScore >= 100) {
+            position = 3;
         }
+        return position;
     }
 }
