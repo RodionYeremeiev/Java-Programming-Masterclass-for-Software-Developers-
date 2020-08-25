3abcd52313ab1754f1cf2b40dd2dfb1a8adae666
Rodion Yeremeiev <rodion.ieremeiev@gmail.com>
2020-08-25 14:36:37 +0300

    47. Methods In Java

--- IfKeywordsAndCodeBloks/src/com/gmail/ryitlearning/Main.java
+++ IfKeywordsAndCodeBloks/src/com/gmail/ryitlearning/Main.java
@@ -3,28 +3,8 @@ package com.gmail.ryitlearning;
 public class Main {
 
     public static void main(String[] args) {
-        boolean gameOver = true;
-//        int score = 800;
-//        int score = 4000;
-        int score = 5000;
-        int levelCompleted = 5;
-        int bonus = 100;
 
-        if (score < 5000 && score > 1000) {
-            System.out.println("Your score was less than 5000 but greater than 1000");
-        } else if (score < 1000) {
-            System.out.println("your score less than 1000");
-        } else {
-            System.out.println("Got here");
-        }
-
-//        if (gameOver == true) {
-        if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
-        }
-//        int savedFinalScore = finalScore;
+        calculateScore(true, 800, 5, 100);
 
         /* Challenge:
         Print out a second score on the screen with the following:
@@ -34,23 +14,32 @@ public class Main {
         But make sure the first printout above still displays as well
          */
 
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
+        calculateScore(true, 10000, 8, 200);
 
-        if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
+//        if (gameOver) {
+//            int finalScore = score + (levelCompleted * bonus);
+//            System.out.println("Your final score was " + finalScore);
+//        }
 
         boolean newGameOver = true;
         int newScore = 10000;
         int newLevelCompleted = 8;
         int newBonus = 200;
 
+        calculateScore(newGameOver, newScore, newLevelCompleted, newBonus);
+    }
+
+    public static int calculateScore(boolean gameOver,
+                                     int score,
+                                     int levelCompleted,
+                                     int bonus) {
         if (gameOver) {
-            int finalScore = newScore + (newLevelCompleted * newBonus);
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
             System.out.println("Your final score was " + finalScore);
+            return finalScore;
+        } else {
+            return -1;
         }
     }
 }
