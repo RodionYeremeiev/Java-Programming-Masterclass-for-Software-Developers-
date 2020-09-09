a55349fcab95f3e8c048ee0fa41d04a09d253297
Rodion Yeremeiev <rodion.ieremeiev@gmail.com>
2020-09-09 16:55:04 +0300

    77. Classes Part 2

--- Classes/src/com/gmail/ryitlearning/Main.java
+++ Classes/src/com/gmail/ryitlearning/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Car porsche = new Car();
         Car holden = new Car();
-        porsche.model = "Carrera";
+        System.out.println("porsch model is " + porsche.getModel());
+        porsche.setModel("911");
+        porsche.setModel("Carrera");
+        System.out.println("porsch model is " + porsche.getModel());
     }
 }
