7fb063d3a005fd4f8c9834bbc765a05911da0903
Rodion Yeremeiev <rodion.ieremeiev@gmail.com>
2020-09-09 16:41:11 +0300

    76. Classes Part 1

---
+++ Classes/src/com/gmail/ryitlearning/Car.java
@@ -0,0 +1,10 @@
+package com.gmail.ryitlearning;
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+}
