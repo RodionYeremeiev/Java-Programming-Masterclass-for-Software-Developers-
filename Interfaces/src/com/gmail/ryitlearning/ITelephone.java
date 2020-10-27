e22f3149f1be1d317dc979cc848546218467d781
Rodion Yeremeiev <rodion.ieremeiev@gmail.com>
2020-10-27 16:00:13 +0200

    122. Interfaces part1

---
+++ Interfaces/src/com/gmail/ryitlearning/ITelephone.java
@@ -0,0 +1,9 @@
+package com.gmail.ryitlearning;
+
+public interface ITelephone {
+    void powerOn();
+    void dial(int phoneNumber);
+    void answer();
+    boolean callPhone(int phoneNumber);
+    boolean isRingning();
+}
