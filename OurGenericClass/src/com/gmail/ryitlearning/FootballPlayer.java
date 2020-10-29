986b5eb9ce93f2b55a82ca430b6ed689525aa28e
Rodion Yeremeiev <rodion.ieremeiev@gmail.com>
2020-10-29 21:05:21 +0200

    136. Our Generics Class

---
+++ OurGenericClass/src/com/gmail/ryitlearning/FootballPlayer.java
@@ -0,0 +1,7 @@
+package com.gmail.ryitlearning;
+
+public class FootballPlayer extends Player{
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
---
+++ OurGenericClass/src/com/gmail/ryitlearning/SoccerPlayer.java
@@ -0,0 +1,7 @@
+package com.gmail.ryitlearning;
+
+public class SoccerPlayer extends Player{
+    public SoccerPlayer(String name) {
+        super(name);
+    }
+}
