0471733798aa41b095492dd7869894803c9d7230
Rodion Yeremeiev <rodion.ieremeiev@gmail.com>
2020-10-29 21:39:39 +0200

    137. Our Generics Class Part 2

--- OurGenericClass/src/com/gmail/ryitlearning/Team.java
+++ OurGenericClass/src/com/gmail/ryitlearning/Team.java
@@ -2,13 +2,13 @@ package com.gmail.ryitlearning;
 
 import java.util.ArrayList;
 
-public class Team {
+public class Team<T extends Player> {
     int played = 0;
     int won = 0;
     int lost = 0;
     int tied = 0;
     private String name;
-    private ArrayList<Player> members = new ArrayList<>();
+    private ArrayList<T> members = new ArrayList<>();
 
     public Team(String name) {
         this.name = name;
@@ -18,7 +18,7 @@ public class Team {
         return name;
     }
 
-    public boolean addPlayer(Player player) {
+    public boolean addPlayer(T player) {
         if (members.contains(player)) {
             System.out.println(player.getName() + " is already in this team");
             return false;
@@ -33,21 +33,39 @@ public class Team {
         return this.members.size();
     }
 
-    public void matchResult(Team opponent, int ourScore, int theirScore) {
+    public void matchResult(Team<T> opponent, int ourScore, int theirScore) {
+        String message;
+
         if (ourScore > theirScore) {
             won++;
+            message = " beat ";
         } else if (ourScore == theirScore) {
             tied++;
+            message = " drew with ";
         } else {
             lost++;
+            message = " lost to ";
         }
         played++;
         if (opponent != null) {
+            System.out.println(this.getName() + message + opponent.getName());
             opponent.matchResult(null, theirScore, ourScore);
         }
     }
 
     public int ranking() {
         return won * 2 + tied;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "played=" + played +
+                ", won=" + won +
+                ", lost=" + lost +
+                ", tied=" + tied +
+                ", name='" + name + '\'' +
+                ", members=" + members +
+                '}';
     }
 }
