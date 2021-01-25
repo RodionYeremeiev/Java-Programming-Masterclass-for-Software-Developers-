72112848eec620c8ee37040f09a1d3af37dd5c78
Rodion Yeremeiev <rodion.ieremeiev@gmail.com>
2021-01-25 12:49:59 +0200

    Coding Exercise 38: Encapsulation

---
+++ CodingExercises/Encapsulation/src/com/gmail/ryitlearning/Printer.java
@@ -0,0 +1,41 @@
+package com.gmail.ryitlearning;
+
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        if (tonerLevel > -1 && tonerLevel <= 100) {
+            this.tonerLevel = tonerLevel;
+        } else {
+            this.tonerLevel = -1;
+        }
+        this.duplex = duplex;
+        this.pagesPrinted = 0;
+    }
+
+    public int addToner(int tonerAmount) {
+        int totalAmount = this.tonerLevel + tonerAmount;
+        if (totalAmount > 100) {
+            return -1;
+        } else if ((tonerAmount > 0 && tonerAmount <= 100) && (totalAmount <= 100)) {
+            return this.tonerLevel = totalAmount;
+        }
+        return -1;
+    }
+
+    public int printPages(int pages) {
+        int pagesToPrint = pages;
+        if (this.duplex) {
+            System.out.println("Printig in duplex mode");
+            pagesToPrint = pages / 2 + pages % 2;
+        }
+        this.pagesPrinted += pagesToPrint;
+        return pagesToPrint;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+}
