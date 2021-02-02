9c0f32ea45f04e1805dcb7e29f9a6ddde05aa76a
Rodion Yeremeiev <rodion.ieremeiev@gmail.com>
2021-02-02 20:00:01 +0200

    Coding Exercise 48: Playlist - Inner Class(code for test compiler added)

--- CodingExercises/InnerClassesPlayList/src/com/gmail/ryitlearning/Album.java
+++ CodingExercises/InnerClassesPlayList/src/com/gmail/ryitlearning/Album.java
@@ -100,18 +100,85 @@ public class Album {
         }
     }
 
+    /**
+     * For test compiler:
+     * import java.util.ArrayList;
+     * import java.util.LinkedList;
+     *
+     * public class Album {
+     * private String name;
+     *     private String artist;
+     *     private SongList songs;
+     *
+     *     public Album(String name, String artist) {
+     *         this.name = name;
+     *         this.artist = artist;
+     *         this.songs = new SongList();
+     *     }
+     *
+     *     public boolean addSong(String title, double duration) {
+     *         return this.songs.add(new Song(title, duration));
+     *     }
+     *
+     *     public boolean addToPlayList(int trackNumber, LinkedList<Song> playlist) {
+     *         Song checkedSong = this.songs.findSong(trackNumber);
+     *         if (checkedSong != null) {
+     *             playlist.add(checkedSong);
+     *             return true;
+     *         }
+     *         System.out.println("This album does not have a track " + trackNumber);
+     *         return false;
+     *     }
+     *
+     *     public boolean addToPlayList(String title, LinkedList<Song> playlist) {
+     *         Song checkedSong = this.songs.findSong(title);
+     *         if (checkedSong != null) {
+     *             playlist.add(checkedSong);
+     *             return true;
+     *         }
+     *         System.out.println("The song " + title + " is not in this album");
+     *         return false;
+     *     }
+     *
+     *
+     *     public static class SongList {
+     *         private ArrayList<Song> songs;
+     *
+     *         private SongList() {
+     *             this.songs = new ArrayList<Song>();
+     *         }
+     *
+     *         private Song findSong(String title) {
+     *             Song song = null;
+     *             for (int i = 0; i < songs.size(); i++) {
+     *                 song = songs.get(i);
+     *                 if (song.getTitle().equals(title)) {
+     *                     return song;
+     *                 }
+     *             }
+     *             return null;
+     *         }
+     *
+     *         private Song findSong(int trackNumber) {
+     *             int index = trackNumber - 1;
+     *             if ((index >= 0) && (index <= songs.size())) {
+     *                 return songs.get(index);
+     *             }
+     *             return null;
+     *         }
+     *
+     *         private boolean add(Song song) {
+     *             if (songs.contains(song)) {
+     *                 System.out.println("-");
+     *                 return false;
+     *             }
+     *             this.songs.add(song);
+     *             System.out.println("+");
+     *             return true;
+     *         }
+     *     }
+     * }
+     */
+
 }
-
-//    private Song findSong(String title){
-//        Song song = null;
-//        for (int i = 0; i < songs.size(); i++) {
-//            song = songs.get(i);
-//            if (song.getTitle().equals(title)){
-//                return song;
-//            }
-//        }
-//        return null;
-//    }
-
-
 
