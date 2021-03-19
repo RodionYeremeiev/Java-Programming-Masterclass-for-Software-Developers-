e10cdead280c4bc99044fe4751ade978dcbe9735
Rodion Yeremeiev <rodion.ieremeiev@gmail.com>
2021-03-19 13:52:16 +0200

    340. .executeQuery() and using Constants (TODO: create a method to remove duplicate INSERT code)

--- DataBases/TestDB/src/com/gmail/ryitlearning/Main.java
+++ DataBases/TestDB/src/com/gmail/ryitlearning/Main.java
@@ -3,21 +3,71 @@ package com.gmail.ryitlearning;
 import java.sql.*;
 
 public class Main {
+    public static final String DB_NAME = "testjava.db";
+    public static final String CONNECTION_STRING = "jdbc:sqlite:F:\\IDE\\Projects\\Udemy\\Java Programming Masterclass for Software Developers\\java-programming-masterclass-for-software-developers\\DataBases\\TestDB\\" + DB_NAME;
+
+    public static final String TABLE_CONTACTS = "contacts";
+
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_PHONE = "phone";
+    public static final String COLUMN_EMAIL = "email";
 
     public static void main(String[] args) {
-        // write your code here
-//        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:F:\\IDE\\Projects\\Udemy\\Java Programming Masterclass for Software Developers\\java-programming-masterclass-for-software-developers\\DataBases\\TestDB\\testjava.db");
-//             Statement statement = connection.createStatement();) {
-//            statement.execute("CREATE TABLE contacts(name TEXT, phone INTEGER, email TEXT)");
 
         try {
-//            Connection connection = DriverManager.getConnection("jdbc:sqlite:F:\\IDE\\Projects\\Udemy\\Java Programming Masterclass for Software Developers\\java-programming-masterclass-for-software-developers\\DataBases\\TestDB\\testjava.db");
-//            Class.forName("org.sqlite.JDBC");
-
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:F:\\IDE\\Projects\\Udemy\\Java Programming Masterclass for Software Developers\\java-programming-masterclass-for-software-developers\\DataBases\\TestDB\\testjava.db");
-//            connection.setAutoCommit(false);
+            Connection connection = DriverManager.getConnection(CONNECTION_STRING);
             Statement statement = connection.createStatement();
-//            statement.execute("CREATE TABLE IF NOT EXISTS contacts(name TEXT, phone INTEGER, email TEXT)");
+//            connection.setAutoCommit(false);
+
+            statement.execute("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+
+            statement.execute("CREATE TABLE IF NOT EXISTS " + TABLE_CONTACTS +
+                    " (" + COLUMN_NAME + " text, " +
+                    COLUMN_PHONE + " integer, " +
+                    COLUMN_EMAIL + " text" +
+                    ")");
+
+            statement.execute("INSERT  INTO " + TABLE_CONTACTS +
+                    " (" + COLUMN_NAME + ", " +
+                    COLUMN_PHONE + ", " +
+                    COLUMN_EMAIL + " ) " +
+                    "VALUES('Rod', 123456, 'Rod@email.com')");
+
+            statement.execute("INSERT  INTO " + TABLE_CONTACTS +
+                    " (" + COLUMN_NAME + ", " +
+                    COLUMN_PHONE + ", " +
+                    COLUMN_EMAIL +
+                    " ) " +
+                    "VALUES('Joe', 123789, 'Joe@email.com')");
+
+            statement.execute("INSERT  INTO " + TABLE_CONTACTS +
+                    " (" + COLUMN_NAME + ", " +
+                    COLUMN_PHONE + ", " +
+                    COLUMN_EMAIL + " ) " +
+                    "VALUES('Bob', 654321, 'Bob@email.com')");
+
+            statement.execute("INSERT  INTO " + TABLE_CONTACTS +
+                    " (" + COLUMN_NAME + ", " +
+                    COLUMN_PHONE + ", " +
+                    COLUMN_EMAIL + " ) " +
+                    "VALUES('Jim', 123789, 'Jim@email.com')");
+
+            statement.execute("UPDATE " + TABLE_CONTACTS + " SET " +
+                    COLUMN_PHONE + "= '753159'" +
+                    " WHERE " + COLUMN_NAME + " ='Jim'");
+
+            statement.execute("DELETE " + " FROM " + TABLE_CONTACTS +
+                    " WHERE " + COLUMN_NAME + " ='Bob'");
+
+            ResultSet results = statement.executeQuery("SELECT * FROM " + TABLE_CONTACTS);
+
+            while (results.next()) {
+                System.out.println(results.getString(COLUMN_NAME) + " " +
+                        results.getInt(COLUMN_PHONE) + " " +
+                        results.getString(COLUMN_EMAIL));
+            }
+
+
 //
 //            statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Rod', 123456, 'Rod@email.com')");
 //            statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Joe', 123789, 'Joe@email.com')");
@@ -25,13 +75,16 @@ public class Main {
 //            statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Jim', 123789, 'Jim@email.com')");
 //            statement.execute("UPDATE contacts SET phone = '753159' WHERE name='Jim'");
 //            statement.execute("DELETE FROM contacts WHERE name='Rod'");
-            statement.execute("SELECT * FROM contacts");
-            ResultSet results = statement.getResultSet();
-            while (results.next()){
-                System.out.println(results.getString("name") + " " +
-                                   results.getInt("phone") + " " +
-                                   results.getString("email"));
-            }
+//            statement.execute("SELECT * FROM contacts");
+//            ResultSet results = statement.getResultSet();
+
+//            ResultSet results = statement.executeQuery("SELECT * FROM contacts");
+
+//            while (results.next()) {
+//                System.out.println(results.getString("name") + " " +
+//                        results.getInt("phone") + " " +
+//                        results.getString("email"));
+//            }
             results.close();
 
             statement.close();
@@ -39,6 +92,9 @@ public class Main {
 
         } catch (SQLException e) {
             System.out.println("Something went wrong " + e.getMessage());
+            e.printStackTrace();
         }
     }
+
+
 }
