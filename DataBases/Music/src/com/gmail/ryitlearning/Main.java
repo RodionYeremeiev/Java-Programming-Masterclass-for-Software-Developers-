db32c70e4cda1cac8226b4f3c267195e82f6b54a
Rodion Yeremeiev <rodion.ieremeiev@gmail.com>
2021-03-19 15:11:07 +0200

    341. The Music SQLite Database

---
+++ DataBases/Music/src/com/gmail/ryitlearning/Main.java
@@ -0,0 +1,15 @@
+package com.gmail.ryitlearning;
+
+import com.gmail.ryitlearning.model.DataSource;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DataSource dataSource = new DataSource();
+        if (!dataSource.open()){
+            System.out.println("Can't open database");
+            return;
+        }
+        dataSource.close();
+    }
+}
