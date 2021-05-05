3ad8f122c68a19f27a94c86bd503577a011b54fd
Rodion Yeremeiev <rodion.ieremeiev@gmail.com>
2021-05-05 21:50:07 +0300

    156. Comparable and Comparator

--- Collections/src/com/gmail/ryitlearning/Theatre.java
+++ Collections/src/com/gmail/ryitlearning/Theatre.java
@@ -1,11 +1,23 @@
 package com.gmail.ryitlearning;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Theatre {
+    static final Comparator<Seat> PRICE_ORDER = new Comparator<Seat>() {
+        @Override
+        public int compare(Seat seat1, Seat seat2) {
+            if (seat1.getPrice() < seat2.getPrice()) {
+                return -1;
+            } else if (seat1.getPrice() > seat2.getPrice()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    };
+
     private final String theatreName;
-    public static List<Seat> seats = new ArrayList<>();
+    private List<Seat> seats = new ArrayList<>();
 
     public Theatre(String theatreName, int numRows, int seatsPerRow) {
         this.theatreName = theatreName;
@@ -13,7 +25,13 @@ public class Theatre {
         int lastRow = 'A' + (numRows - 1);
         for (char row = 'A'; row <= lastRow; row++) {
             for (int seatNum = 1; seatNum <= seatsPerRow; seatNum++) {
-                Seat seat = new Seat(row + String.format("%02d", seatNum));
+                double price = 12.00;
+                if ((row < 'D') && (seatNum >= 4 && seatNum <= 9)) {
+                    price = 14;
+                } else if ((row > 'F') || (seatNum < 4) || seatNum > 9) {
+                    price = 7.00;
+                }
+                Seat seat = new Seat(row + String.format("%02d", seatNum), price);
                 seats.add(seat);
             }
         }
@@ -23,66 +41,54 @@ public class Theatre {
         return theatreName;
     }
 
-//    public boolean reserveSeat(String seatNumber) {
-//        Seat requestedSeat = new Seat(seatNumber);
-//        int foundSeat = Collections.binarySearch(seats, requestedSeat, null);
-//        if (foundSeat >= 0) {
-//            return seats.get(foundSeat).reserve();
-//        } else {
-//            System.out.println("There is no seat " + seatNumber);
-//            return false;
-//        }
-////        for (Seat seat : seats) {
-////            System.out.print(".");
-////            if (seat.getSeatNumber().equals(seatNumber)) {
-////                requestedSeat = seat;
-////                break;
-////            }
-////        }
-////        if (requestedSeat == null) {
-////            System.out.println("There is no seat " + seatNumber);
-////            return false;
-////        }
-////        return requestedSeat.reserve();
-//    }
-
-    public boolean reserveSeat(String seatNumber) { //binary search impl
-        int low = 0;
-        int high = seats.size() - 1;
-
-        while (low <= high) {
-            System.out.print("."); // dots printed to visualize how many iterations will be done
-            int mid = (low + high) / 2;
-            Seat midVal = seats.get(mid);
-            int cmp = midVal.getSeatNumber().compareTo(seatNumber);
-
-            if (cmp < 0) {
-                low = mid + 1;
-            } else if (cmp > 0) {
-                high = mid - 1;
-            } else {
-                return seats.get(mid).reserve();
-            }
+    public boolean reserveSeat(String seatNumber) {
+        Seat requestedSeat = new Seat(seatNumber, 0);
+        int foundSeat = Collections.binarySearch(seats, requestedSeat, null);
+        if (foundSeat >= 0) {
+            return seats.get(foundSeat).reserve();
+        } else {
+            System.out.println("There is no seat " + seatNumber);
+            return false;
         }
-        System.out.println("There is no seat " + seatNumber);
-        return false;
     }
+
+//    public boolean reserveSeat(String seatNumber) { //binary search impl
+//        int low = 0;
+//        int high = seats.size() - 1;
+//
+//        while (low <= high) {
+//            System.out.print("."); // dots printed to visualize how many iterations will be done
+//            int mid = (low + high) / 2;
+//            Seat midVal = seats.get(mid);
+//            int cmp = midVal.getSeatNumber().compareTo(seatNumber);
+//
+//            if (cmp < 0) {
+//                low = mid + 1;
+//            } else if (cmp > 0) {
+//                high = mid - 1;
+//            } else {
+//                return seats.get(mid).reserve();
+//            }
+//        }
+//        System.out.println("There is no seat " + seatNumber);
+//        return false;
+//    }
 
 
     //for testing
-    public void getSeats() {
-        for (Seat seat : seats) {
-            System.out.println(seat.getSeatNumber());
-        }
+    public Collection<Seat> getSeats() {
+        return seats;
     }
 
     public class Seat implements Comparable<Seat> {
 
         private final String seatNumber;
+        private double price;
         private boolean reserved = false;
 
-        public Seat(String seatNumber) {
+        public Seat(String seatNumber, double price) {
             this.seatNumber = seatNumber;
+            this.price = price;
         }
 
         @Override
@@ -112,6 +118,10 @@ public class Theatre {
 
         public String getSeatNumber() {
             return seatNumber;
+        }
+
+        public double getPrice() {
+            return price;
         }
     }
 }
